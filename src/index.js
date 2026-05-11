524c34cbf1f91d020815c6d981f61d66268452f7
alejandroct21 <alecardozat@gmail.com>
2026-05-10 22:16:38 -0500

    feature: design layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+const modal = document.getElementById('modal');
+
+const addBtn = document.querySelector('.add-btn');
+const cancelBtn = document.querySelector('.btn-cancel');
+
+
+addBtn.addEventListener('click', ()=>showModal());
+cancelBtn.addEventListener('click', ()=>closeModal());
+
+
+function showModal(){
+    modal.classList.add('active');
+}
+
+function closeModal(){
+    modal.classList.remove('active');
+}
